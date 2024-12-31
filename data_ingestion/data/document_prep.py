e6906a7d988e816429b7c83224a23174c072c4a3
Trevor Royer <troyer@redhat.com>
2024-12-31 12:59:55 -0700

    add create data folders

--- data_ingestion/data/document_prep.py
+++ data_ingestion/data/document_prep.py
@@ -1,8 +1,24 @@
+from pathlib import Path
+
+
+def create_data_folders(data_folder_location: str = "./data") -> Path:
+    data_folder = Path(data_folder_location)
+    raw_folder = data_folder.joinpath("raw")
+    processed_folder = data_folder.joinpath("processed")
+
+    raw_folder.mkdir(parents=True, exist_ok=True)
+    processed_folder.mkdir(parents=True, exist_ok=True)
+
+    return data_folder
+
+
 def download_document():
     pass
+
 
 def convert_pdf_to_markdown():
     pass
 
+
 if __name__ == "__main__":
-    pass
+    create_data_folders()
