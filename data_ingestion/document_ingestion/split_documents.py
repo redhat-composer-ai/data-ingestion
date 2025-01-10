775187a755a4eb0752ec3875e4bd70a88fcc0d50
Trevor Royer <troyer@redhat.com>
2025-01-10 16:41:53 -0700

    create document splitter class

---
+++ data_ingestion/document_ingestion/split_documents.py
@@ -0,0 +1,14 @@
+from langchain_core.documents import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+class DocumentSplitter:
+    def __init__(self, documents: list[Document]):
+        self.documents = list(documents)
+        self.chunk_size = 1024
+        self.chunk_overlap = 40
+        self.splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
+
+    def splits(self) -> list[Document]:
+        document_splits = self.splitter.split_documents(self.documents)
+        return document_splits
