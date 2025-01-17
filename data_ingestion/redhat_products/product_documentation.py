19c041aa7e4dbfd58ff10fa1b1f3868052df3db7
Trevor Royer <troyer@redhat.com>
2025-01-17 10:49:15 -0700

    update to use new component structure

---
+++ data_ingestion/redhat_products/product_documentation.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from typing import Tuple, Union
+
+from langchain_core.documents import Document
+
+from data_ingestion.redhat_products.redhat_product_documentation import RedHatProductDocumentation
+
+def get_documents_list(product: str, version: str, language: str = "en") -> list[str]:
+    rh_product = RedHatProductDocumentation(product=product, version=version, language=language)
+    product_doc_pages = rh_product.product_pages()
+    return product_doc_pages
---
+++ data_ingestion/redhat_products/product_documentation_component.py
@@ -0,0 +1,15 @@
+import os
+
+from kfp import dsl
+from kfp.dsl import Artifact
+
+image = os.getenv("COMPONENT_IMAGE", "quay.io/redhat-composer-ai/data-ingestion")
+tag = os.getenv("COMPONENT_IMAGE_TAG", "latest")
+
+@dsl.component(target_image=f"{image}:{tag}")
+def get_documents_list_component(product: str, version: str, language: str) -> list[str]:
+    from data_ingestion.redhat_products.product_documentation import get_documents_list
+
+    documents_list = get_documents_list(product=product, version=version, language=language)
+
+    return documents_list
