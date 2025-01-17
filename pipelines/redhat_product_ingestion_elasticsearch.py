67df70f902edde411028f27c26aa02e426c79cae
Trevor Royer <troyer@redhat.com>
2025-01-17 16:04:06 -0700

    update pipeline setup

--- pipelines/redhat_product_ingestion_elasticsearch.py
+++ pipelines/redhat_product_ingestion_elasticsearch.py
@@ -11,11 +11,11 @@ from kfp.dsl import Artifact
 # hacky way to add the project root to be able to import the module
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from data_ingestion.redhat_products.ingestion import get_product_documents
+from data_ingestion.redhat_products.product_documentation_component import get_documents_list_component
 
 @kfp.dsl.pipeline()
 def ingestion_pipeline(product: str = "red_hat_openshift_ai_self-managed", version: str = "2.16", language: str= "en"):
-    get_documents_list_task = get_product_documents(product=product, version=version, language=language)
+    get_documents_list_task = get_documents_list_component(product=product, version=version, language=language)
     # convert_to_markdown_task = convert_to_markdown(raw_document=get_documents_list_task.output)
     # ingest_document(document=convert_to_markdown_task.output)
 
