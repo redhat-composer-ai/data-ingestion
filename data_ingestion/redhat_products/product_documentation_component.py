669c05b530ad2cf8166a535679f3a5bbad18f117
Trevor Royer <troyer@redhat.com>
2025-01-18 05:33:59 -0700

    Remove list[str] due to https://github.com/kubeflow/pipelines/issues/11536

--- data_ingestion/redhat_products/product_documentation_component.py
+++ data_ingestion/redhat_products/product_documentation_component.py
@@ -7,7 +7,7 @@ tag = os.getenv("COMPONENT_IMAGE_TAG", "latest")
 
 
 @dsl.component(target_image=f"{image}:{tag}")
-def get_documents_list_component(product: str, version: str, language: str) -> list[str]:
+def get_documents_list_component(product: str, version: str, language: str) -> list:
     from data_ingestion.redhat_products.product_documentation import get_documents_list
 
     documents_list = get_documents_list(product=product, version=version, language=language)
